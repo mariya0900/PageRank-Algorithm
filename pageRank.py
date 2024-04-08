877687c63557e3fe2aaf12b543a61768e2ae1a58
mariya0900 <maria.anashkina@ontariotechu.net>
2024-04-08 16:40:44 -0400

    local tiny change

--- pageRank.py
+++ pageRank.py
@@ -12,27 +12,28 @@ def bruteForce(x: np.array, P: np.array, threshold: int):
     for i in range(num_pages):
         rank.append([x[i]])
     # rank = np.append(rank, x)
-    # loop = True
-    # while loop:
-    #     x_new = np.matmul(P, x)
-    #     iter += 1
-    #     iteration.append(iter)
-    #     if np.linalg.norm(x_new - x, ord=1) < threshold:
-    #         loop = False
-    #     x = x_new
-    #     rank = np.append(rank, x)
-    for _ in range(100):
+    loop = True
+    for i in range (100):
         x_new = np.matmul(P, x)
-        for i in range(num_pages):
-            rank[i].append(x_new[i])
-
-        diff = x_new - x
-        print(np.linalg.norm(diff))
-        if np.linalg.norm(diff) < threshold:
-            iter = _
+        iter += 1
+        
+        if np.linalg.norm(x_new - x, ord=1) < threshold:
+            loop = False
             break
-
         x = x_new
+        rank = np.append(rank, x)
+    # for _ in range(100):
+    #     x_new = np.matmul(P, x)
+    #     for i in range(num_pages):
+    #         rank[i].append(x_new[i])
+
+    #     diff = x_new - x
+    #     #print(np.linalg.norm(diff))
+    #     if np.linalg.norm(diff) < threshold:
+    #         iter = _
+    #         break
+
+    #     x = x_new
 
     return x, iter, rank
 
@@ -48,11 +49,12 @@ def eigenSolver(P:np.array)->np.array:
 
 x = init(6)
 P = np.array(((0, 0, 1/3, 0, 0, 0), (1/2, 0, 1/3, 0, 0, 0), (1/2, 1, 0, 0, 0, 0), (0, 0, 0, 0, 1/2, 1),
-              (0, 0, 1/3 , 1/2, 0 , 0), (0, 0, 0, 1, 0, 0)))
+              (0, 0, 1/3 , 1/2, 0 , 0), (0, 0, 0, 1/2, 1/2, 0)))
+print(P)
 print(f"x = {x}")
 # print(f"P = \n{P}")
 x, min_iter, rank = bruteForce(x, P, 0.1)
-print(f"x = {np.sum(x)}")
+print(f"x = {x}")
 # print(len(iteration))
 temp = np.linalg.eig(P.T)
 # print(temp[0].real)
